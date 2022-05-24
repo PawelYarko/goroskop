d36df8e3a12c359f81f8ae6357645a3cdbc91448
Pawel Yarko <pawelyarko@gmail.com>
2022-05-24 22:23:21 +0200

    form

--- src/index.js
+++ src/index.js
@@ -2,12 +2,12 @@ import './sass/main.scss';
 
 const refs ={
     form: document.querySelector('.main__form'),
-    footerElStart: document.querySelector('.main__footer--start'),
-    footerElEnd: document.querySelector('.main__footer--end'),
+    footerEl: document.querySelector('.main__footer'),
     btnContainer: document.querySelector('.form__button--container'),
     radioBtns: document.querySelectorAll('.form__radio-btn'),
     formBtn: document.querySelector('.form__button'),
-    mainContaer: document.querySelector('.main__content'),
+    mainContainer: document.querySelector('.main__content'),
+    timeContainer: document.querySelector('.time__container'),
 }
 
 let sex = '';   
@@ -15,11 +15,7 @@ let sex = '';
 refs.form.addEventListener('change', onRadioBtnCkick);
 refs.form.addEventListener('submit', onBtnSubmitClick);
 
-function newFooter(){
-    refs.footerElStart.classList.toggle('is-active');
-    refs.btnContainer.classList.toggle('is-open');
-    refs.footerElEnd.classList.toggle('is-active'); 
-}
+
 
 function onRadioBtnCkick(e){
     e.preventDefault();
@@ -28,7 +24,8 @@ function onRadioBtnCkick(e){
     for(const radioBtn of refs.radioBtns){
         if(radioBtn.checked){
             sex = radioBtn.value;
-            newFooter();
+            refs.btnContainer.classList.add('is-open');
+            refs.footerEl.classList.toggle('main__footer--end');
         }
     }
 }
@@ -36,7 +33,9 @@ function onRadioBtnCkick(e){
 function onBtnSubmitClick(e){
     e.preventDefault();
     // console.log(sex);
-    refs.mainContaer.classList.add('is-active'); 
+    refs.mainContainer.classList.add('is-active'); 
+    refs.timeContainer.classList.remove('is-active');
+
     
 
 }

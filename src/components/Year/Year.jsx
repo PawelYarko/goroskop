fdb693fdcf2d537e83d60feda8beedb83023dcff
PawelYarko <pawelyarko@gmail.com>
2022-08-18 21:02:21 +0200

    Update Year.jsx

--- src/components/Year/Year.jsx
+++ src/components/Year/Year.jsx
@@ -1,29 +1,48 @@
 
+import { Link, useLocation } from 'react-router-dom';
+import { useState } from 'react';
+import ButtonBack from '../ButtonBack/ButtonBack';
+import Radio from '@mui/material/Radio';
+import RadioGroup from '@mui/material/RadioGroup';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import FormControl from '@mui/material/FormControl';
+import FormLabel from '@mui/material/FormLabel';
+import Button from '@mui/material/Button';
+import s from '../Home/Home.module.css';
 
-const Year = () => {
+
+const Year = () => {  
+  const[year, setYear] = useState(null);
+  const location = useLocation();
+
+  const backLinkHref = location.state?.from ?? '/';
 
     return(
-        <>
-        <span>Loader</span>
-        <form >
-              <h3>Какой Вы видите свою жизнь через 5 лет?</h3>
-              <label>
-                <input type="radio"/> 
-                Брак, семья, дети, дом                
-              </label>
-              <label>
-                <input type="radio"/> 
-                Путешествия по Миру                
-              </label>
-              <label>
-                <input type="radio"/> 
-                Успешная карьера                 
-              </label>
-              <label>
-                <input type="radio"/> 
-                Всё вместе                 
-              </label>
-          </form>
+      <>
+      <h3>Какой Вы видите свою жизнь через 5 лет?</h3>
+      <ButtonBack to={backLinkHref} />
+      <span>Loader</span>
+      <FormControl>
+        <FormLabel id="demo-radio-buttons-group-label" />
+        <RadioGroup
+          aria-labelledby="demo-radio-buttons-group-label"
+          defaultValue="female"
+          name="radio-buttons-group"
+          onChange={e => setYear(e.target.value)}
+        >
+          <FormControlLabel value="family" control={<Radio />} label="Брак, семья, дети, дом" />
+          <FormControlLabel value="travel" control={<Radio />} label="Путешествия по Миру" />
+          <FormControlLabel value="career" control={<Radio />} label="Успешная карьера" />
+          <FormControlLabel value="all" control={<Radio />} label="Всё вместе" />
+        </RadioGroup>
+      </FormControl>
+      {year && (
+        <Link className={s.button} to="/birthday">
+          <Button type="submit" variant="contained">
+            Далее
+          </Button>
+        </Link>
+      )}
         </>
     );
     }

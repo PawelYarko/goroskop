8d9a4d4d338f22a3a874efb2f2f0a03b848a28ea
PawelYarko <pawelyarko@gmail.com>
2022-08-22 22:36:44 +0200

    add btn back

--- src/components/CheckValue/CheckValue.jsx
+++ src/components/CheckValue/CheckValue.jsx
@@ -1,10 +1,14 @@
 import { Link } from 'react-router-dom';
+import ButtonBack from '../ButtonBack/ButtonBack';
 import Button from '@mui/material/Button';
 import s from '../Home/Home.module.css';
 
 const CheckValue = () =>{
+  const location = useLocation();
+  const backLinkHref = location.state?.from ?? "/";
     return(
         <>
+          <ButtonBack to={backLinkHref} />
         <h1>CheckValue</h1>
         <Link className={s.button} to="/night" ><Button type="submit" variant="contained">Далее</Button></Link>
         <Link className={s.button} to="/birthday">
--- src/components/GetResult/GetResult.jsx
+++ src/components/GetResult/GetResult.jsx
@@ -1,10 +1,14 @@
 import { Link } from 'react-router-dom';
+import ButtonBack from '../ButtonBack/ButtonBack';
 import Button from '@mui/material/Button';
 import s from '../Home/Home.module.css';
 
 const GetResult = () =>{
+  const location = useLocation();
+  const backLinkHref = location.state?.from ?? "/";
     return(
         <>
+        <ButtonBack to={backLinkHref} />
         <h1>Get result</h1>
         <Link className={s.button} to="/birthday">
           <Button type="submit" variant="contained">
